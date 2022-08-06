60ba6bc67ecea9857a96ec908b58356a849f35c0
R Greene <greenbert81@gmail.com>
2022-08-06 08:11:01 -0700

    reservation and customer router.get

--- routes/apiRoutes/customerRoutes/index.js
+++ routes/apiRoutes/customerRoutes/index.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
 
+const {
+    Customer
+} = require('../../../models');
+
+router.get('/', async (req, res) => {
+    try{
+        const customers = await Customer.findAll();
+        res.json(customers);
+    } catch (error) {
+        res.status(500).json({ error });
+    }  
+});
+
+
+router.get('/:customerId', async (req, res) => {
+    try{
+        const customer = await Customer.findByPk(req.params.customerId);
+
+        if (!customer) {
+            return res.status(404).json({ error: "No Matching Customer Found!"});
+        }
+        res.json(customer);
+    } catch (error) {
+        res.status(500).json({ error });
+    }  
+});
 
 module.exports = router;
--- routes/apiRoutes/reservationRoutes/index.js
+++ routes/apiRoutes/reservationRoutes/index.js
@@ -1,4 +1,33 @@
 const router = require('express').Router();
 
+const {
+    Reservation
+} = require('../../../models');
+
+router.get('/', async (req, res) => {
+    try{
+        const reservations = await Reservation.findAll();
+        res.json(reservations);
+    } catch (error) {
+        res.status(500).json({ error });
+    }  
+});
+
+
+router.get('/:reservationId', async (req, res) => {
+    try{
+        const reservation = await Reservation.findByPk(req.params.reservationId);
+
+        if (!reservation) {
+            return res.status(404).json({ error: "No Matching Reservation Found!"});
+        }
+        res.json(reservation);
+    } catch (error) {
+        res.status(500).json({ error });
+    }  
+});
 
 module.exports = router;
+
+
+
